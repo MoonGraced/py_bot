92f06187310a75034cf908bdca44e9152c4bd485
Кирилл Андреевич Герасимов <talathel@vk.com>
2025-11-08 12:17:09 +0300

    typo fixes

--- vk.py
+++ vk.py
@@ -35,7 +35,7 @@ class VKAPI:
         self.base_url = "https://api.live.vkvideo.ru"
         self.token = None
         self.token_expires = None
-        self.piv_lobby = {s: self.check_streamer_by_url(s) for s in load_channels_from_json('piv_lobby_streamers.json')}
+        self.piv_lobby = {s: self.check_streamer_by_url(s)['data']['channel'] for s in load_channels_from_json('piv_lobby_streamers.json')}
         self.timer = datetime.now()
 
     def get_token(self):
